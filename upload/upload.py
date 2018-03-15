6b86341ca0ae80be7bb65b1abd41a1fe1564e77f
Scott Fisher <scott.fisher@ucop.edu>
2018-03-15 10:52:44 -0700

    changes to script to expect 201 response from hub server when uploading

--- upload/upload.py
+++ upload/upload.py
@@ -4,6 +4,9 @@ from urllib.parse import urljoin
 import io
 #import ipdb; ipdb.set_trace()
 
+class UploadException(Exception):
+    pass
+
 def send_to_datacite():
     my_url = urljoin(config.hub_base_url, 'reports')
     if config.output_format == 'json':
@@ -13,9 +16,13 @@ def send_to_datacite():
     headers = {'content-type': ct, 'Authorization': f'Bearer {config.hub_api_token}'}
     with io.open(f'{config.output_file}.{config.output_format}', 'r', encoding='utf-8') as myfile:
         data = myfile.read()
+
     # this is how you do a direct post of the information.
     # For a multipart encoded file, see http://docs.python-requests.org/en/master/user/quickstart/
-    # It's not clear to me which way they want it, as multi-part encoded file or as a direct POST of the json/tsv
+    # It's not clear to me which way: as multi-part encoded file or as a direct POST of the json/tsv data
     response = requests.post(my_url, data=data.encode("utf-8"), headers=headers)
-    print(response)
-    print('sent?')
+    print('')
+    print('Sending report to the hub')
+    print(f'Response was: {response}')
+    if response.status_code != 201:
+        raise UploadException("Expected to get 201 status code when posting the report to the hub")
