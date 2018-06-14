e6879192c13a7a28a8023c452f2943fb65f6a196
Scott Fisher <scott.fisher@ucop.edu>
2018-06-14 16:13:37 -0700

    avoiding unreliable datacite api with retries for 500 errors

--- upload/upload.py
+++ upload/upload.py
@@ -5,6 +5,7 @@ import io
 import json
 from urllib.request import pathname2url
 import sys
+import time
 #import ipdb; ipdb.set_trace()
 
 class UploadException(Exception):
@@ -21,16 +22,22 @@ def save_response(response):
     # file.write(json.dumps(parsed, indent=2, sort_keys=True))
     file.close()
 
+# this method is a retry since the datacite api randomly gives many 500 errors
+def retry_if_500(method, url, data, headers):
+    for attempt in range(60):
+        response = getattr(requests, method)(url, data=data.encode("utf-8"), headers=headers)
+        if response.status_code < 500 or attempt == 59:
+            return response
+        else:
+            print(f'expected to upload, but got code {response.status_code}')
+            time.sleep(1)
+
 def send_to_datacite():
     if config.output_format == 'json':
         ct = 'application/json; charset=UTF-8'
     else:
         ct = 'text/tab-separated-values; charset=UTF-8'
     headers = {'content-type': ct, 'Authorization': f'Bearer {config.hub_api_token}'}
-
-    # TODO: if we need to test, use this below because still not accepting grids
-    # with io.open('good_test.json', 'r', encoding='utf-8') as myfile:
-    #    data = myfile.read()
 
     with io.open(f'{config.output_file}.{config.output_format}', 'r', encoding='utf-8') as myfile:
         data = myfile.read()
@@ -39,14 +46,16 @@ def send_to_datacite():
     my_id = config.current_id()
     if my_id is None:
         my_url = urljoin(config.hub_base_url, 'reports')
-        response = requests.post(my_url, data=data.encode("utf-8"), headers=headers)
+        # response = requests.post(my_url, data=data.encode("utf-8"), headers=headers)
+        response = retry_if_500(method='post', url=my_url, data=data, headers=headers)
         save_response(response)
         json_data = json.loads(response.text)
         if 'report' in json_data:
             config.write_id(json_data['report']['id'])
     else:
         my_url = urljoin(config.hub_base_url, f'reports/{pathname2url(my_id)}')
-        response = requests.put(my_url, data=data.encode("utf-8"), headers=headers)
+        # response = requests.put(my_url, data=data.encode("utf-8"), headers=headers)
+        response = retry_if_500(method='put', url=my_url, data=data, headers=headers)
         save_response(response)
 
     if response.status_code < 200 or response.status_code > 299:
