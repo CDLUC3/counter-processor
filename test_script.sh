f44c4303e0932d9d4b5ea59900e0a925f86c79cb
Scott Fisher <scott.fisher@ucop.edu>
2020-11-09 10:26:36 -0800

    Updates  for Python 3.7.9, however now the dates for processing aren't calculated correctly anymore.

--- test_script.sh
+++ test_script.sh
@@ -4,7 +4,7 @@ rm state/statefile.json || true
 STARTTIME=$(date +%s)
 echo "Starting"
 YEAR_MONTH="2020-09" LOG_NAME_PATTERN="/Users/sfisher/counter-test-files/counter_(yyyy-mm-dd).log_combined" \
-    UPLOAD_TO_HUB=False SIMULATE_DATE="2020-09-06" ./main.py
+    UPLOAD_TO_HUB=False SIMULATE_DATE="2020-09-01" ./main.py
 echo "Done"
 ENDTIME=$(date +%s)
 echo "It took $(($ENDTIME - $STARTTIME)) seconds to complete..."
